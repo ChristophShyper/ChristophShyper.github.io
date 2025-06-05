406c757e4a88c6955e271e59eed48eb0c44bfc47
ChristophShyper <45788587+ChristophShyper@users.noreply.github.com>
2025-06-05 11:26:53 +0200

    :zap: Revert missing icons

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -5,21 +5,98 @@ export interface Language {
 }
 
 export const languages: Record<string, Language> = {
+  angular: {
+    name: "Angular",
+    iconName: "angular",
+  },
+  astro: {
+    name: "Astro",
+    iconName: "astro",
+  },
+  bootstrap: {
+    name: "Bootstrap",
+    iconName: "bootstrap",
+  },
   cloudflare: {
     name: "Cloudflare",
     iconName: "cloudflare",
+  },
+  html: {
+    name: "HTML 5",
+    iconName: "html",
+  },
+  javascript: {
+    name: "JavaScript",
+    iconName: "javascript",
+  },
+  mongo: {
+    name: "MongoDb",
+    iconName: "mongo",
+  },
+  mysql: {
+    name: "MySQL",
+    className: "!bg-[#f6ece1]",
+    iconName: "mysql",
+  },
+  wordpress: {
+    name: "Wordpress",
+    iconName: "wordpress",
+  },
+  node: {
+    name: "Node.js",
+    iconName: "node",
+  },
+  tailwind: {
+    name: "Tailwind CSS",
+    iconName: "tailwind",
+  },
+  figma: {
+    name: "Figma",
+    iconName: "figma",
+  },
+  firebase: {
+    name: "Firebase",
+    iconName: "firebase",
   },
   markdown: {
     name: "Markdown",
     iconName: "markdown",
   },
+  php: {
+    name: "PHP",
+    iconName: "php",
+  },
+  sass: {
+    name: "Sass",
+    iconName: "sass",
+  },
+  ts: {
+    name: "TypeScript",
+    iconName: "typescript",
+  },
   git: {
     name: "Git",
     iconName: "git",
   },
-  github: {
-    name: "GitHub",
-    iconName: "github",
+  css: {
+    name: "CSS",
+    iconName: "css",
+  },
+  vercel: {
+    name: "Vercel",
+    iconName: "vercel",
+  },
+  netlify: {
+    name: "Netlify",
+    iconName: "netlify",
+  },
+  gatsby: {
+    name: "Gatsby",
+    iconName: "gatsby",
+  },
+  windsurf: {
+    name: "Windsurf",
+    iconName: "windsurf-logo",
   },
   cursor: {
     name: "Cursor",
@@ -32,6 +109,10 @@ export const languages: Record<string, Language> = {
   python: {
     name: "Python",
     iconName: "python",
+  },
+  github: {
+    name: "GitHub",
+    iconName: "github",
   },
   terraform: {
     name: "Terraform",
